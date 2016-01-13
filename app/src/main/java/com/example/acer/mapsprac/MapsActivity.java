c29b325c3b5775e847c575df976ba38b270aaeea
terrelewis <terrellewis@hotmail.com>
2016-01-13 16:01:34 +0530

    Updated class

--- app/src/main/java/com/example/acer/mapsprac/MapsActivity.java
+++ app/src/main/java/com/example/acer/mapsprac/MapsActivity.java
@@ -8,6 +8,10 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -18,20 +22,64 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     TextView myAddress;
     TextView service;
+    JSONArray obj=null;
+    private static final String TAG = "FRAGMENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        String tag_json_arry = "json_array_req";
+
+        String url = "http://fabfresh.elasticbeanstalk.com/users/postalcode/";
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.e(TAG, response.toString()+"");
+                        if(response != null && response.length() > 6)
+                        {
+
+                            obj = new JSONArray();
+                            obj=response;
+                            //getData();
+
+                        } else {
+                            Log.e(TAG, "obj is null");
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage() + "");
+
+            }
+        }){ @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("Authorization", "Bearer hari");
+
+            return headers;
+        }};
+
+        AppController.getInstance().addToRequestQueue(req, tag_json_arry);
+
+
+        //getData();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -51,6 +99,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
+
+
         mMap = googleMap;
 
 
@@ -58,18 +110,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         service=(TextView)findViewById(R.id.textView1);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.setMyLocationEnabled(true);
-
+        /*Location location=mMap.getMyLocation();
+      final LatLng CURLOC = new LatLng(15, 15);*/
 
         mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
             @Override
             public void onMapLoaded() {
-                //Log.e("TAG", googleMap.getCameraPosition().target.toString());
+               // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CURLOC, 15));
+                Log.e("asd", mMap.getCameraPosition().target.toString());
 
-                Toast.makeText(
-                        MapsActivity.this,
-                        "Lat " + mMap.getCameraPosition().target.latitude + " "
-                                + "Long " + mMap.getCameraPosition().target.longitude,
-                        Toast.LENGTH_LONG).show();
+
 
             }
         });
@@ -78,85 +128,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onCameraChange(CameraPosition cameraPosition) {
 
                 Log.e("asd", String.valueOf(cameraPosition.target.latitude) + "");
-                if (cameraPosition.target.latitude != 0 && cameraPosition.target.longitude != 0) {
-                    getMyLocationAddress((cameraPosition.target.latitude), (cameraPosition.target.longitude));
+                if (cameraPosition.target.latitude != 0 && cameraPosition.target.longitude != 0 &&cameraPosition.target.latitude>-90 && cameraPosition.target.latitude<90 && cameraPosition.target.longitude>-180 &&cameraPosition.target.longitude<180) {
 
+                    try {
+                        getMyLocationAddress((cameraPosition.target.latitude), (cameraPosition.target.longitude));
+                    }catch(IndexOutOfBoundsException e){Toast.makeText(
+                            MapsActivity.this,
+                            "No service" ,
+                            Toast.LENGTH_LONG).show();}
                 }
             }
         });
 
     }
 
-
-
-
-
-    public void getMyLocationAddress(double lat, double longi) {
+  public void getMyLocationAddress(double lat, double longi) {
 
         int flag=0;
         Geocoder geocoder= new Geocoder(this, Locale.ENGLISH);
-        JSONArray obj=null;
-        try {
-            obj = new JSONArray("[ {\n" +
-                    "    \"postalCode\": 560034,\n" +
-                    "    \"Locality\": \"Kormangala\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560030,\n" +
-                    "    \"Locality\": \"Koramangala\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560095,\n" +
-                    "    \"Locality\": \"Koramangala\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560068,\n" +
-                    "    \"Locality\": \"Koramangala\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560078,\n" +
-                    "    \"Locality\": \"JP Nagar\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560041,\n" +
-                    "    \"Locality\": \"Jayanagar\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560076,\n" +
-                    "    \"Locality\": \"BTM\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560029,\n" +
-                    "    \"Locality\": \"BTM 1\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560070,\n" +
-                    "    \"Locality\": \"Jayanagar west\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560069,\n" +
-                    "    \"Locality\": \"Jayanagar east\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560011,\n" +
-                    "    \"Locality\": \"Jayanagar lil block\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560047,\n" +
-                    "    \"Locality\": \"koramangala\"\n" +
-                    "  },\n" +
-                    "  {\n" +
-                    "    \"postalCode\": 560102,\n" +
-                    "    \"Locality\": \"HSR\"\n" +
-                    "  }]");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+      List<Address> addresses=null;
         try {
 
             //Place your latitude and longitude
-            List<Address> addresses = geocoder.getFromLocation(lat,longi, 1);
-
+           try {
+                addresses = geocoder.getFromLocation(lat, longi, 1);
+           }catch(IndexOutOfBoundsException e){Toast.makeText(
+                   MapsActivity.this,
+                   "No service" ,
+                   Toast.LENGTH_LONG).show();}
             if(addresses != null) {
 
                 Address fetchedAddress = addresses.get(0);
@@ -169,7 +168,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 String str=strAddress.toString();
                 String s=str.substring(str.length()-7, str.length()-1);
                 for (int i = 0; i < obj.length(); i++) {
-                    JSONObject jsonobject = null;
+                    JSONObject jsonobject;
                     try {
                         jsonobject = obj.getJSONObject(i);
                         String postal = jsonobject.getString("postalCode");
@@ -188,8 +187,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if(flag==0)
                     service.setText("Service unavailable");
 
-
-
             }
 
             else
@@ -202,5 +199,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             Toast.makeText(getApplicationContext(), "Could not get address..!", Toast.LENGTH_LONG).show();
         }
     }
+
 
 }
